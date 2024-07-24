0baf67423d975b8f95eff41c31393d7a8a095bc6
best-ed <edanelunalo7@gmail.com>
2024-07-24 15:07:00 +0300

    Updated about me

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,16 +9,13 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+            My name is Edwin Lungatso, and I am passionate about technology and its potential to transform lives. With a strong background in cloud computing and networking, I am an incoming Software Engineering Fellow at Headstarter AI. My curiosity extends to the dynamic field of cybersecurity, where I continuously seek to expand my knowledge and skills. Driven by a commitment to innovation and excellence, I am eager to contribute to cutting-edge projects and collaborate with like-minded professionals.
           </p>
-          <p className="hero--section-description">
+          {/* <p className="hero--section-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
             officiis sit debitis omnis harum sed veniam quasi dicta accusamus
             recusandae?
-          </p>
+          </p>  */}
         </div>
       </div>
     </section>
